4a3a40bfab6215d80ca439683d13300615a1e500
barbz <eduardob907@gmail.com>
2025-03-16 10:17:21 +0100

    tech: remove credit card validation

--- src/main/java/com/playtomic/tests/wallet/domain/valueobject/CreditCard.java
+++ src/main/java/com/playtomic/tests/wallet/domain/valueobject/CreditCard.java
@@ -5,8 +5,5 @@ public record CreditCard(String number) {
         if (number == null) {
             throw new IllegalArgumentException("Credit card number cannot be null");
         }
-        if (number.length() != 16) {
-            throw new IllegalArgumentException("Credit card number must be 16 digits long");
-        }
     }
 }
